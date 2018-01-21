f6ad2467928524f9a5946a5b8e90f9e06b759a7f
Meninblack007 <sanyam.53jain@gmail.com>
2018-01-21 16:21:27 +0530

    msm8998-common: Build SnapdragonCamera

--- common.mk
+++ common.mk
@@ -138,7 +138,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@3.2-impl \
-    Snap
+    SnapdragonCamera
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
